e55310fcce5643bf2269ebd19edc2ded8c1429bf
Oliver Geschine <oli.geschine@web.de>
2026-03-01 15:41:34 +0100

    add styling to portfolio section

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ function toggleLanguageIcon() {
     document.getElementById('de_active')?.classList.toggle('dNone');
     document.getElementById('en_active')?.classList.toggle('dNone');
 }
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let projectContainer = document.querySelector('.project_container');
+
+//     if (projectContainer) {
+//         projectContainer.addEventListener('mouseover', function () {
+//             console.log("maus ist über dem element");
+
+//             document.getElementById('overlay')?.classList.remove('dNone');
+//             document.getElementById('overlay')?.classList.add('overlay');
+//             document.getElementById('projectImg')?.classList.remove('project-container-img');
+//             document.getElementById('projectImg')?.classList.add('zoomed');
+//         });
+
+//         projectContainer.addEventListener('mouseout', function () {
+//             document.getElementById('overlay')?.classList.add('dNone');
+//             document.getElementById('overlay')?.classList.remove('overlay');
+//             document.getElementById('projectImg')?.classList.remove('zoomed');
+//             document.getElementById('projectImg')?.classList.add('project-container-img');
+//         });
+//     }
+// });
